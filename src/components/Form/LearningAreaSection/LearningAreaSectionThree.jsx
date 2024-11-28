afb9fb7cd40d282717ae0fd7b4909ed1efea21cb
Moazzem-Bhuiyan <tiyonbhuiyam.201@gmail.com>
2024-11-28 11:08:29 +0600

    fix

--- src/components/Form/LearningAreaSection/LearningAreaSectionThree.jsx
+++ src/components/Form/LearningAreaSection/LearningAreaSectionThree.jsx
@@ -346,6 +346,7 @@ const LearningAreaSectionThree = ({ register, setValue }) => {
             groupKey="improvements"
             hideMinusFor={[
               "French",
+              "Mathematics",
               "Foreign language",
               "Historical knowledge",
               "Participation",
